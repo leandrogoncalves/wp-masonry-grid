a81c686a232175b541fc153eac2f258312c21ecd
leandrogoncalves <contato.leandrogoncalves@gmail.com>
2016-09-09 16:46:26 -0300

    change functionality

--- public/WP_Masonry_Grid_Static.php
+++ public/WP_Masonry_Grid_Static.php
@@ -5,52 +5,87 @@
  */
 class WP_Masonry_Grid_Static
 {
+    /**
+     * Get POST and GET streams and sanitize the data
+     * @return array|mixed
+     */
     public static function getInput(){
         $inputs = [];
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-            $args = array(
+            $args = [
                 'wpmg'   => [
                     'filter' => FILTER_SANITIZE_STRING,
                     'flags'  => FILTER_REQUIRE_ARRAY
                 ]
-            );
+            ];
 
             $inputs = filter_input_array(INPUT_POST, $args);
         }
 
-
         if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-
-
             $inputs['pg'] = filter_input(INPUT_GET, 'pg', FILTER_VALIDATE_INT);
         }
 
         return $inputs;
-
     }
 
+    /**
+     * Check if the value from stream is equals the input value
+     * @param $name
+     * @param $value
+     * @return string
+     */
     public static function checked($name, $value){
         $inputs =  self::getInput();
         $checado = false;
 
         $field = explode('-',$name);
 
-        if(array_key_exists($field[1], $inputs['wpmg'])){
-            if(array_key_exists($field[2], $inputs['wpmg']['filter'])){
-                switch ($field[2]){
-                    case 'title' :
-                        $checado = $inputs['wpmg']['filter']['title'] == $value ? true : false;
-                        break;
-                    case 'letter' :
-                        $checado = $inputs['wpmg']['filter']['letter'] == $value ? true : false;
-                        break;
+        if(!empty($inputs)){
+            if(array_key_exists($field[1], $inputs['wpmg'])){
+                if(array_key_exists($field[2], $inputs['wpmg']['filter'])){
+                    switch ($field[2]){
+                        case 'letter' :
+                            $checado = $inputs['wpmg']['filter']['letter'] == $value ? true : false;
+                            break;
+                    }
+                }
+                if($field[1] == 'tax'){
+                    if(is_array($inputs['wpmg']['tax'])){
+                        foreach ($inputs['wpmg']['tax'] as $taxonomia) {
+                            if(is_array($taxonomia)){
+                                $checado = in_array($value,$taxonomia) ? true : false;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        return $checado ? ' checked="checked" ' : '';
+        return $checado ? " checked='checked' " : '';
     }
 
+
+    /**
+     * Get the value of text entry sent by form
+     * @param $name
+     * @return mixed
+     */
+    public static function getValue($name){
+        $inputs = self::getInput();
+        $field  = explode('-',$name);
+
+        if(!empty($inputs)){
+            if(array_key_exists($field[1], $inputs['wpmg'])){
+                if(array_key_exists($field[2], $inputs['wpmg']['filter'])){
+                    return  $inputs['wpmg']['filter']['title'];
+                }
+            }
+        }
+    }
+
+
+    
 }
